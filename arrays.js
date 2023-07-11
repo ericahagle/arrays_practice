4a6238e23d72cf0e5ca63b2cd19537ee4cf312db
Erica Hagle <ericahagle.dev@gmail.com>
2023-07-10 22:54:40 -0700

    Add variables for string, number, and boolean arrays

---
+++ arrays.js
@@ -0,0 +1,8 @@
+// Declare a variable that stores an Array of at least 4 Strings.
+var strs = ["this", "has", "four", "strings"];
+
+// Declare a variable that stores an Array of at least 4 Numbers.
+var nums = [1, 2, 3, 4];
+
+// Declare a variable that stores an Array of at least 4 Booleans.
+var bools = [true, false, true, false];
