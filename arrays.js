482c6ffd9a2b9b404de1b26a089bbcaf47c2a096
Erica Hagle <ericahagle.dev@gmail.com>
2023-07-10 23:05:14 -0700

    Apply methods, explain them, and print out the result

--- arrays.js
+++ arrays.js
@@ -1,8 +1,17 @@
 // Declare a variable that stores an Array of at least 4 Strings.
-var strs = ["this", "has", "four", "strings"];
+var strs = ["this", "array", "has", "four"];
+// Using .push() with a new string inside the () will add that string to the end of the array. Or 'push' it onto the end of the array.
+strs.push("strings");
+console.log(strs);
 
 // Declare a variable that stores an Array of at least 4 Numbers.
 var nums = [1, 2, 3, 4];
+// Using .shift() will remove the first element of the array and 'shift' over the array.
+nums.shift();
+console.log(nums);
 
 // Declare a variable that stores an Array of at least 4 Booleans.
 var bools = [true, false, true, false];
+// Using .pop() with a new boolean inside the () will remove that boolean from the end of the array. Aka 'pop' it off the end.
+bools.pop(true);
+console.log(bools);
